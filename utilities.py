a5de48b6256fc6d80d0e28797350b58ebba1c701
xyloforce <fabsassolas@outlook.fr>
2020-11-01 11:29:52 +0100

    working script with functions

--- utilities.py
+++ utilities.py
@@ -1,13 +1,33 @@
-species = dict()
+def create_neighborhoods(species_dict):
+    neighbourhood = dict() # each char and his neighbors
+    for i in species_dict: # i is a species
+        for j in species_dict[i]: # j is a state of i
+            if j not in neighbourhood: # ensure proper initialization
+                neighbourhood[j]=set()
+            neighbourhood[j].update(species[i]) # add states from the same species to j' neighbors
+    return neighbourhood
+    
+def dict_to_mzn_array(neighbourhood_dict):
+    final_table="[" # start of array in mzn
+    for i in neighbourhood_dict:
+        final_table +="|" # start of line in mzn array
+        for j in neighbourhood_dict:
+            if j in neighbourhood_dict[i]:
+                final_table += "TRUE, "
+            else:
+                final_table += "FALSE, "
+        final_table += "|"
+    final_table += "];"
+    return final_table
+
+species = dict() # entry data
 species["S1"]="P1", "Z2", "C3", "F4"
 species["S2"]="C1", "A2", "V3", "F4"
 species["S3"]="A1", "A2", "M3", "F4"
 species["S4"]="A1", "C2", "M3", "A4"
 
-neighbourhood = dict()
+final_table=dict_to_mzn_array(create_neighborhoods(species))
 
-for i in species:
-    for j in species[i]:
-        if j not in neighbourhood:
-            neighbourhood[j]=set()
-        j.update(species[i])
+result=open("table_mzn.txt", "w")
+result.write(final_table);
+result.close()
