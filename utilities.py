4c6c45f8f4a7bd507624c64b0e4afb6ba6e32eaa
xyloforce <fabsassolas@outlook.fr>
2020-11-04 21:22:48 +0100

    added creation of incompatibilities table

--- utilities.py
+++ utilities.py
@@ -6,6 +6,24 @@ def create_neighborhoods(species_dict):
                 neighbourhood[j]=set()
             neighbourhood[j].update(species[i]) # add states from the same species to j' neighbors
     return neighbourhood
+
+def create_incompatibilities(species_dict):
+    incompatibility = dict() # each char and his same characters
+    pos_by_char = dict()
+    for i in species_dict:
+        for j in species_dict[i]: # j is a state in the species i
+            index = species_dict[i].index(j)
+            if j not in pos_by_char: # ensure proper initialization
+                pos_by_char[j] = set()
+            pos_by_char[j].add(index) # add value to set in order to avoid duplicates 
+    for i in pos_by_char:
+        if i not in incompatibility:
+            incompatibility[i]=set()
+        for j in pos_by_char:
+            if i != j and pos_by_char[i]==pos_by_char[j]:
+                incompatibility[i].add(j)
+    print(incompatibility)
+    return incompatibility
     
 def dict_to_mzn_array(neighbourhood_dict):
     final_table="[" # start of array in mzn
@@ -31,16 +49,16 @@ def dict_keys_to_mzn_enum(neighbourhood_dict):
     return enum
 
 species = dict() # entry data
-species["S1"]="P1", "Z2", "C3", "F4"
-species["S2"]="C1", "A2", "V3", "F4"
-species["S3"]="A1", "A2", "M3", "F4"
-species["S4"]="A1", "C2", "M3", "A4"
+species["S1"]="A1", "B1", "C1"
+species["S2"]="A1", "B2", "C1"
 
 neighbourhood = create_neighborhoods(species)
 final_table=dict_to_mzn_array(neighbourhood)
 enum = dict_keys_to_mzn_enum(neighbourhood)
+incompatibility = dict_to_mzn_array(create_incompatibilities(species))
 
 result=open("table_mzn.txt", "w")
 result.write(final_table + "\n");
+result.write(incompatibility + "\n");
 result.write(enum)
 result.close()
