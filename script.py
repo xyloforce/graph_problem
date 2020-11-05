ba6c943b616792228c6e12fce30344a399cdd6f9
celine <celine.mandier@etu.univ-lyon1.fr>
2020-11-05 20:11:55 +0100

    Mise en place conversion séquence AA+position

---
+++ script.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#Faire Blast à partir d'une séquence de protéine d'une espèce de façon à trouver des séquences 
+#homologues de façon a récupérer d'autres séquences de cette protéines pour d'autres espèces. 
+
+##Initialization and file opening  
+dico = {}
+with open('raw','r') as file:
+	position = 1 #initialiser la position
+	sequences = [] 
+	specie = []
+	sequence = ""
+	for line in file: 
+		if line.startswith(">"): #première ligne avec nom d'espèce
+			element = line.rstrip().split('[') #on récupère le nom de l'espèce /!\ faut enlever le ] en plus
+			specie.append(element[1]) #on stock dans une liste les noms des espèces
+		else:
+			while line[0]!=">": #pour les lignes de séquence
+				for l in line:
+					lp=l+str(position)# Associe à l'AA sa position dans la séquence
+					print(lp)
+					sequence += lp #Normalement doit faire une chaine de caractère avec AA + position
+					position+=1 #incrémente position de +1
+			sequences.append(lp)		
+			print(sequence)
+
+
