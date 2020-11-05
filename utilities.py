280a721565a649a9c0cdd5a789908f2e3675955e
xyloforce <fabsassolas@outlook.fr>
2020-11-05 19:05:57 +0100

    added utilities to allow complete datafile creation

--- utilities.py
+++ utilities.py
@@ -22,11 +22,10 @@ def create_incompatibilities(species_dict):
         for j in pos_by_char:
             if i != j and pos_by_char[i]==pos_by_char[j]:
                 incompatibility[i].add(j)
-    print(incompatibility)
     return incompatibility
     
-def dict_to_mzn_array(neighbourhood_dict):
-    final_table="[" # start of array in mzn
+def dict_to_mzn_array(neighbourhood_dict, name):
+    final_table= name + " = [" # start of array in mzn
     for i in neighbourhood_dict:
         final_table +="| " # start of line in mzn array
         for j in neighbourhood_dict:
@@ -41,24 +40,43 @@ def dict_to_mzn_array(neighbourhood_dict):
     return final_table
 
 def dict_keys_to_mzn_enum(neighbourhood_dict):
-    enum="{"
+    enum="States_available = {"
     for i in neighbourhood_dict:
         enum += i + ","
     enum = enum[:-1]
     enum += "};"
     return enum
 
+def dict_keys_to_mzn_set(neighbourhood_dict):
+    mzn_set="number_States = 1.."
+    mzn_set += str(len(neighbourhood_dict))
+    mzn_set += ";"
+    return mzn_set
+
+def dict_keys_to_mzn_axis(neighbourhood_dict, axis_number):
+    mzn_axis = "axe" + axis_number + " = "
+    keys=list()
+    for i in neighbourhood_dict:
+        keys.append(i)
+    mzn_axis += keys[0]
+    mzn_axis += ".."
+    mzn_axis += keys[-1]
+    mzn_axis += ";"
+    return mzn_axis
+
 species = dict() # entry data
-species["S1"]="A1", "B1", "C1"
-species["S2"]="A1", "B2", "C1"
+species["S1"]="A1", "D2", "B3", "B4"
+species["S2"]="B1", "C2", "D3", "B4"
+species["S3"]="C1", "C2", "F3", "Z4"
 
+result=open("Problem.dzn", "w")
 neighbourhood = create_neighborhoods(species)
-final_table=dict_to_mzn_array(neighbourhood)
-enum = dict_keys_to_mzn_enum(neighbourhood)
-incompatibility = dict_to_mzn_array(create_incompatibilities(species))
 
-result=open("table_mzn.txt", "w")
-result.write(final_table + "\n");
-result.write(incompatibility + "\n");
-result.write(enum)
+result.write(dict_keys_to_mzn_enum(neighbourhood) + "\n")
+result.write(dict_to_mzn_array(neighbourhood, "share_species") + "\n")
+result.write(dict_to_mzn_array(create_incompatibilities(species), "incompatibilities") + "\n")
+result.write(dict_keys_to_mzn_set(neighbourhood) + "\n")
+result.write(dict_keys_to_mzn_axis(neighbourhood, "1") + "\n")
+result.write(dict_keys_to_mzn_axis(neighbourhood, "2") + "\n")
+
 result.close()
