060013ebbb74f830693895896bfe371da7fb1238
celine <celine.mandier@etu.univ-lyon1.fr>
2020-11-07 21:24:27 +0100

    Dico espece:sequence fini

--- script.py
+++ script.py
@@ -6,27 +6,28 @@
 #homologues de façon a récupérer d'autres séquences de cette protéines pour d'autres espèces. 
 
 ##Initialization and file opening  
-dico = {}
+
 with open('raw','r') as file:
-	position = 1 #initialiser la position
-	sequences = [] 
-	specie = []
+	dico = {}
 	sequence = ""
-	lp = ""
 	for line in file: 
 		line = line.rstrip()
-		if line.startswith(">"): #première ligne avec nom d'espèce
-			element = line.replace('[', ';').replace(']', ';').split(';') #on récupère le nom de l'espèce
+		if line.startswith(">"): 
+			#On traite la première ligne du bloc de l'espèce 
+			element = line.replace('[', ';').replace(']', ';').split(';') #On récupère le nom de l'espèce
 			espece = element[1]
-			dico[espece] = ""
-			#print(dico) #on stock dans une liste les noms des espèce
+			dico[espece] = "" #On la stock en clé de dico
 			position = 1
+			sequence = ""
+			lp = ""
+
 		else:
+			#On traite le bloc de séquence
 			for l in line:
 				lp=l+str(position)# Associe à l'AA sa position dans la séquence
-				sequence += lp #Normalement doit faire une chaine de caractère avec AA + position
+				sequence += lp 
 				position+=1 #incrémente position de +1
-			dico[espece] = sequence
+			dico[espece] = sequence #Dico avec clé espèce et valeur associée
 	print(dico)
 
 	
